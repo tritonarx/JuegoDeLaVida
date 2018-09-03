5686d1ee152df2d7b84ccd4085f894de8b8d14bd
lucascabot95 <42903915+lucascabot95@users.noreply.github.com>
2018-09-02 21:01:14 -0300

    Update Example curses.py

--- Example curses.py
+++ Example curses.py
@@ -1,24 +1,27 @@
-def main():
-  import curses
-	stdscr = curses.initscr();
-	running=True;
-	stdscr.addstr("\n Para comenzar ingrese 'b' (Begin) | para finalizar ingrese 'q' (Exit)\n  =>");
-	while(running):	
-		key = stdscr.getch();
-		if (chr(key) == "q"):
-			running = False;
-		if (chr(key) == "b"):
-			curses.noecho(); 
-			stdscr.addstr("\n Welcome to Game");
-			load_data();
-			vida();
-			print_load_data()
-		else:
-			stdscr.addstr("\n Para comenzar ingrese 'b' (Begin) | para finalizar ingrese 'q' (Exit)\n  =>");
-	curses.nocbreak();
-	stdscr.keypad (0);
-	curses.echo();
-	curses.endwin();
-	return 0;
-if (__name__== "__main__"):	
-	main();
+	def main():
+		import curses
+		stdscr = curses.initscr();
+		running=True;
+		stdscr.addstr("\n Para comenzar ingrese 'b' (Begin) | para finalizar ingrese 'q' (Exit)\n  =>");
+		while(running):	
+			key = stdscr.getch();
+			if (chr(key) == "q"):
+				running = False;
+			if (chr(key) == "b"):
+				curses.noecho(); 
+				load_data();
+				vida();
+				print_load_data()
+				key = stdscr.getch();
+				running=False;
+			else:
+				stdscr.addstr("\n Para comenzar ingrese 'b' (Begin) | para finalizar ingrese 'q' (Exit)\n  =>");
+		curses.nocbreak();
+		stdscr.keypad (0);		curses.endwin();
+
+		curses.echo();
+		return 0;
+	if (__name__== "__main__"):	
+		main();
+
+JuegoDeLaVida()
